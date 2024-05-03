13b07fe8f7cc3301455e7bc596c017944d3542f8
gabrielhz <gabrielramoscardoso@hotmail.com>
2024-05-02 21:02:37 -0300

    chore: Add tsnd as developmnet dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    extends: '@rocketseat/eslint-config/node',
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
 ];
